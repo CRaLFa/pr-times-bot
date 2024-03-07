bdb84b876df1e67a2d5308b832a7a477b1f2fef4
morita <morita.kohei@ryobi.co.jp>
2024-03-07 10:06:39 +0900

    rename

--- main.ts
+++ main.ts
@@ -30,12 +30,12 @@ const KV_KEY = ['PR-TIMES-RSS', 'AI', 'published'];
     const feed = await fetchRss();
     // await kv.delete(KV_KEY);
     const lastPublished = (await kv.get<number>(KV_KEY)).value ?? 0;
-    const newAiFeeds = feed.entries.filter((entry) => lastPublished < Date.parse(entry.publishedRaw!) && /\W(AI|ＡＩ)\W/.test(entry.title?.value!));
-    if (newAiFeeds.length < 1) {
-      console.log('No new feed about AI');
+    const newAiEntries = feed.entries.filter((entry) => lastPublished < Date.parse(entry.publishedRaw!) && /\W(AI|ＡＩ)\W/.test(entry.title?.value!));
+    if (newAiEntries.length < 1) {
+      console.log('No new entry about AI');
       return;
     }
-    for (const entry of newAiFeeds) {
+    for (const entry of newAiEntries) {
       const published = new Date(entry.publishedRaw!).toLocaleString();
       for (const channelId of channelIds) {
         await bot.helpers.sendMessage(channelId, {
